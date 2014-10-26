a7f0c2e51a472d0720c5a1dae3e6965a5bcf14ee
Mohammed AlGhannam <mohammed.alghannam@gmail.com>
2014-10-26 10:09:00 +0300

    first commit
    all files are added

---
+++ 2D platformer/Assets/Sample Assets/2D/Scripts/HitterController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitterController : MonoBehaviour {
+
+	Rigidbody player;
+	void start (){
+		player = GetComponent<Rigidbody> ();
+		player.AddForce (Vector2 (5, 0));
+	}
+
+
+
+	void FixedUpdate () {
+	
+		player.AddForce (Vector2 (5, 0));
+
+	}
+}
---
+++ Kicker 2D/Assets/PlayerController.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+var MoveUp : KeyCode;
+var MoveDown : KeyCode;
+var MoveLeft : KeyCode;
+var MoveRight : KeyCode;
+
+
+var speed : float = 10;
+
+function Update ()
+{
+	if (Input.GetKey(MoveUp))
+	{
+		rigidbody2D.velocity.y = speed;
+		//rigidbody2D.AddForce(10f);
+	}
+	else if (Input.GetKey(MoveDown))
+	{
+		rigidbody2D.velocity.y = speed * -1;
+	}
+	else if (Input.GetKey(MoveLeft))
+	{
+		rigidbody2D.velocity.x = speed * -1;
+	}
+	else if (Input.GetKey(MoveRight))
+	{
+		rigidbody2D.velocity.x = speed;
+	}
+	else
+	{
+		rigidbody2D.velocity.y = 0;
+		rigidbody2D.velocity.x = 0;
+	}
+
+
+}
